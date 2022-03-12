ecdaa04c31884dbc4b6dbd58e4cb5d5d608d5a7c
Ariel <arielanus1980@yahoo.com.ar>
2022-03-12 12:21:02 -0300

    subo archivos HTML y JS

---
+++ variables.js
@@ -0,0 +1,16 @@
+var cantidadDeVentanasEnMiCasa= 5
+console.log (cantidadDeVentanasEnMiCasa);
+var soyHumano= true
+console.log (soyHumano);
+var miGustoDePizzaPreferido= "muzzarella"
+console.log (miGustoDePizzaPreferido);
+var unica= false
+console.log (unica);
+var unica= "Hola"
+console.log (unica);
+var unica= 0
+console.log (unica);
+var unica= "Minions"
+console.log (unica);
+
+
